97780087b79f27777c5d61b20486e774c0cc46df
Pichilichi <garmacfco@gmail.com>
2020-11-13 09:27:52 +0100

    Ya funciona

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // En el siguiente ejercicio debéis sacar por consola (console.log) una lista con el nombre de los usuarios.
 // Dicha la lista la obtendréis del documento "users.json" haciendo uso de la función "fetch". Una vez tengáis el array de usuarios, debéis generar otro array con la función "filter" para que solo estén los usuarios que tengán más de 500 en la propiedad "money".
 // Teniendo la lista ya filtrada, la recorreréis con el método "forEach" para mostrar por consola el nombre de cada usuario.
-
 let buttonShowList = document.getElementById("showList");
 buttonShowList.addEventListener("click", getUsers);
 
@@ -14,9 +13,12 @@ function filterUsers(arrayToFiltr){
     return e.money > 500;
   });
 
-  console.log(peopleFiltered);
   
-  peopleFiltered.forEach(function())
+  //console.log(peopleFiltered);
+  
+  peopleFiltered.forEach(function (b){
+   console.log(b.name);
+ });
 
 }
 
